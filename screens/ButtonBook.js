29b80260ccef06ecd04fb616c9aec2a6c6808532
iiConick <connor.mcelroy@ontariotechu.net>
2023-04-26 21:54:30 -0400

    Added firebase read to display the button information in a list format

--- screens/ButtonBook.js
+++ screens/ButtonBook.js
@@ -1,93 +1,13 @@
-// ButtonBook.js
-import React, { useState, useEffect } from 'react';
-import { Text, View, FlatList, StyleSheet, Dimensions } from 'react-native';
-import { db, storage } from '../firebaseConfig';
-
-function ImageDisplay({ imagePath }) {
-  const [imageUrl, setImageUrl] = useState(null);
-
-  useEffect(() => {
-    async function fetchImageUrl() {
-      const imageRef = storage.ref(imagePath);
-      const url = await imageRef.getDownloadURL();
-      setImageUrl(url);
-    }
-
-    fetchImageUrl();
-  }, [imagePath]);
-
-  if (imageUrl) {
-    return <Image source={{ uri: imageUrl }} style={{ width: 200, height: 200 }} />;
-  } else {
-    return null;
-  }
-}
+// ButtonGuide.js
+import React from 'react';
+import { Text, View } from 'react-native';
 
 function ButtonBook() {
-  const [buttons, setButtons] = useState([]);
-
-  useEffect(() => {
-    const unsubscribe = db
-      .collection('buttons')
-      .onSnapshot((snapshot) => {
-        const buttonData = [];
-        snapshot.forEach((doc) => {
-          buttonData.push({ ...doc.data(), id: doc.id });
-        });
-        setButtons(buttonData);
-      });
-
-    return () => {
-      unsubscribe();
-    };
-  }, []);
-
-  const renderItem = ({ item }) => (
-    <View style={styles.buttonContainer}>
-      <Text style={styles.buttonTitle}>{item.name}</Text>
-      <Text style={styles.buttonDescription}>{item.description}</Text>
-      <ImageDisplay imagePath={item.imageUrl} />
-    </View>
-  );
-
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={buttons}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        contentContainerStyle={styles.listContainer}
-      />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Button Book Screen</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  listContainer: {
-    flexGrow: 1,
-    justifyContent: 'center',
-  },
-  buttonContainer: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
-    width: Dimensions.get('window').width - 20,
-  },
-  buttonTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  buttonDescription: {
-    fontSize: 14,
-  },
-});
 
 export default ButtonBook;
--- screens/ButtonGuide.js
+++ screens/ButtonGuide.js
@@ -1,14 +1,97 @@
-// ButtonGuide.js
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, FlatList, StyleSheet, Dimensions, Image } from 'react-native';
+import { db, storage } from '../firebaseConfig';
+
 
 function ButtonGuide() {
+  const [buttons, setButtons] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection('buttons')
+      .onSnapshot((snapshot) => {
+        const buttonData = [];
+        snapshot.forEach((doc) => {
+          buttonData.push({ ...doc.data(), id: doc.id });
+        });
+        setButtons(buttonData);
+      });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={styles.buttonContainer}>
+      <Image
+        source={{ uri: `${item.imageUrl}` }}
+        style={styles.buttonImage}
+      />
+      <View style={styles.textContainer}>
+        <Text style={styles.buttonTitle}>{item.name}</Text>
+        <Text style={styles.buttonDescription}>{item.description}</Text>
+      </View>
+    </View>
+  );
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Button Guide Screen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={buttons}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.listContainer}
+      />
     </View>
   );
 }
 
-export default ButtonGuide;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 40,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  listContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    width: Dimensions.get('window').width - 20,
+  },
+  buttonTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  buttonDescription: {
+    fontSize: 14,
+  },
+  buttonImage: {
+    width: 100,
+    height: 100,
+    resizeMode: 'cover',
+    marginRight: 10, // Add some spacing between the image and the text
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row', // Make the container display its children in a row
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    width: Dimensions.get('window').width - 20,
+  },
+});
 
+export default ButtonGuide;
